6105791649a2f97699757cc0adda63e8d388a943
arthur <arthurdedsec@gmail.com>
2025-09-10 15:59:21 -0300

    Fix mobile view heroes cards and view

--- src/app/home/heroes/components/CardsHeroes.jsx
+++ src/app/home/heroes/components/CardsHeroes.jsx
@@ -10,15 +10,15 @@ export default function CardsHeroes({
     imageAlt
 }) {
     return (
-        <div className="md:flex-col md:flex md:items-center md:justify-center md:text-center items-center flex flex-col p-2   bg-white shadow rounded-xl md:p-5 md:gap-5 md:h-[400px]  ">
+        <div className="md:flex-col md:flex md:items-center md:justify-center md:text-center items-center flex flex-col p-4 h-full   bg-white shadow rounded-xl md:p-5 md:gap-5 md:h-[400px]   ">
             <Image src={imageSrc}
                 alt={imageAlt}
                 width={imageWidth}
                 height={imageHeight}
-                className=" rounded-full"
+                className=" rounded-full w-1/2"
             />
 
-            <div className="gap-2 flex flex-col">
+            <div className="gap-4 flex flex-col">
                 <h1 className="md:text-2xl self-center">{name}</h1>
                 <p className="text-[#22B286] self-center text-center  md:max-w-xs ">{role}</p>
                 <p className="text-[#667085] md:text-md md:max-w-xs text-center" >{description}</p>
